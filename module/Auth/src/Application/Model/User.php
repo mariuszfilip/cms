07c98f97501c23066803bc8f5e1eee831bdf29cf
Mariusz Filipkowski <mariusz24245@gmail.com>
2015-02-14 12:59:14 +0100

    MF -  err

---
+++ module/Auth/src/Application/Model/User.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: mariusz
+ * Date: 14.02.15
+ * Time: 12:33
+ * To change this template use File | Settings | File Templates.
+ */
+
+namespace Auth\Model;
+
+class User{
+
+
+
+}
